c17913e8ee19bbdc1ba0d6fa74b12eeef2f3cd05
Kevin Yee <kevinyee1993@gmail.com>
2018-03-27 18:29:06 -0700

    set up project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/entry.jsx',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
