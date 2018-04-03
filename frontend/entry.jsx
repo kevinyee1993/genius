0bd91fcf221b502ed875a2946a0209987f7394dd
Kevin Yee <kevinyee1993@gmail.com>
2018-04-03 12:05:21 -0700

    setup project wrong need to reset

--- frontend/entry.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  ReactDOM.render(<h1>Hello</h1>, root);
-});
