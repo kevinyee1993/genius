76dde72a72522939c50f49c6d767c73ddc4e1888
Kevin Yee <kevinyee1993@gmail.com>
2018-04-05 11:01:40 -0700

    Style more components

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -37,28 +37,6 @@ renderErrors() {
   );
 }
 
-
-//clear the this.props.errors of all errors
-//call renderErrors again
-//what datatype is errors?
-//call this whenenever user exits modal
-//check modal.jsx, can you have more than one action happen for the
-//onclick of one event?
-
-//errors is a read only property
-//errors is a state so the only way you can change is using an action
-//need to create a removeErrors action that changes the state
-//if create that action, don't actually need to do anything in this file
-//that's a session_action
-//wait you do have an action that handles errors called
-//receiveErrors(errors) - dispatch this whenever you exit a modal
-
-//below is just tester code - delete this after everything works
-// removeErrors() {
-//   this.props.errors = [];
-//   this.renderErrors();
-// }
-
 render() {
 
     return (
@@ -78,21 +56,29 @@ render() {
 
             <div className="login-form">
               <br/>
-              <label>Username:
-                  <input type="text"
-                  value={this.state.username}
-                  onChange={this.update('username')}
-                  className="login-input"
-                />
-              </label>
+
+              <section class="input-body">
+                <label>Username:
+                    <input type="text"
+                    value={this.state.username}
+                    onChange={this.update('username')}
+                    className="login-input"
+                  />
+                </label>
+              </section>
+
               <br/>
-              <label>Password:
-                <input type="password"
-                  value={this.state.password}
-                  onChange={this.update('password')}
-                  className="login-input"
-                />
-              </label>
+
+              <section class="input-body">
+                <label>Password:
+                  <input type="password"
+                    value={this.state.password}
+                    onChange={this.update('password')}
+                    className="login-input"
+                  />
+                </label>
+              </section>
+              
               <br/>
               <input className="session-submit" type="submit" value={this.props.formType} />
             </div>
