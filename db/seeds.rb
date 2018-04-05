6b2ca3b11aec74f78c2b564fe813e40704fc6239
Kevin Yee <kevinyee1993@gmail.com>
2018-04-05 12:34:20 -0700

    Change seed to have correct credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'Demo', password: 'password')
+User.create(username: 'demo', password: 'password')
