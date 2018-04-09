6060f8c6cc0a202823b06310fbe66951a28d37fb
Kevin Yee <kevinyee1993@gmail.com>
2018-04-09 00:47:40 -0700

    Style edit lyrics page and submit buttons

--- frontend/components/tracks/edit_track_lyrics.jsx
+++ frontend/components/tracks/edit_track_lyrics.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 class EditTrackLyrics extends React.Component {
   constructor(props) {
@@ -25,14 +25,17 @@ class EditTrackLyrics extends React.Component {
     return (
       <div>
 
-        <form onSubmit={ this.handleSubmit }>
+        <form onSubmit={ this.handleSubmit } className="edit-track-lyrics-form">
 
           <textarea
             value={ this.state.lyrics }
-            onChange = { this.update('lyrics') } />
+            onChange = { this.update('lyrics') }
+            className="lyric-field"/>
 
-          <input type="submit" value="Save" />
-
+          <div className="track-info-submit-cancel">
+            <input type="submit" value="Save" className="edit-track-info-submit"/>
+            <Link to={`/tracks/${this.props.track.id}`}>Cancel</Link>
+          </div>
         </form>
 
       </div>
