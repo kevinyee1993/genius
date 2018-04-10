b466ee50fa65ebfb55838a9a76069c473b99615f
Kevin Yee <kevinyee1993@gmail.com>
2018-04-10 15:38:49 -0700

    Add dispatch receiveErrors to annotation actions

--- frontend/actions/annotation_actions.js
+++ frontend/actions/annotation_actions.js
@@ -7,40 +7,55 @@ export const RECEIVE_ANNOTATION_ERRORS = 'RECEIVE_ANNOTATION_ERRORS';
 
 export const fetchAllAnnotations = (trackId) => dispatch => (
   AnnotationApiUtil.fetchAllAnnotations(trackId)
-  .then(annotations => receiveAllAnnotations(annotations))
-);
+  .then(annotations => receiveAllAnnotations(annotations)
+), err => (
+  dispatch(receiveErrors(err.responseJSON))
+));
 
 export const fetchAnnotation = (trackId, annotationId) => dispatch => (
   AnnotationApiUtil.fetchAnnotation(trackId, annotationId)
-  .then(annotation => dispatch(receiveAnnotation(annotation)))
-);
+  .then(annotation => dispatch(receiveAnnotation(annotation))
+), err => (
+  dispatch(receiveErrors(err.responseJSON))
+));
 
 export const createAnnotation = (trackId, annotation) => dispatch => (
   AnnotationApiUtil.createAnnotation(trackId, annotation)
-  .then(annotation => dispatch(receiveAnnotation(annotation)))
-);
+  .then(annotation => dispatch(receiveAnnotation(annotation))
+), err => (
+  dispatch(receiveErrors(err.responseJSON))
+));
 
 export const updateAnnotation = (trackId, annotation) => dispatch => (
   AnnotationApiUtil.updateAnnotation(trackId, annotation)
-  .then(annotation => dispatch(receiveAnnotation(annotation)))
-);
+  .then(annotation => dispatch(receiveAnnotation(annotation))
+), err => (
+  dispatch(receiveErrors(err.responseJSON))
+));
 
 export const deleteAnnotation = (trackId, annotationId) => dispatch => (
   AnnotationApiUtil.deleteAnnotation(trackId, annotationId)
-  .then(annotation => removeAnnotation(annotationId))
-);
+  .then(annotation => removeAnnotation(annotationId)
+), err => (
+  dispatch(receiveErrors(err.responseJSON))
+));
 
-const receiveAllAnnotations = annotations => ({
+export const receiveAllAnnotations = annotations => ({
   type: RECEIVE_ALL_ANNOTATIONS,
   annotations
 });
 
-const receiveAnnotation = (annotation) => ({
+export const receiveAnnotation = (annotation) => ({
   type: RECEIVE_ANNOTATION,
   annotation
 });
 
-const removeAnnotation = (annotationId) => ({
+export const removeAnnotation = (annotationId) => ({
   type: REMOVE_ANNOTATION,
   annotationId
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ANNOTATION_ERRORS,
+  errors
 });
