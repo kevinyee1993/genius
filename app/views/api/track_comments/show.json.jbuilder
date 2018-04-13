018bcf82a0cafa46b9239e0fffa9c06cb8a4a1b8
Kevin Yee <kevinyee1993@gmail.com>
2018-04-12 18:20:42 -0700

    Got unupvote working but cannot reupvote afterwards

--- app/views/api/track_comments/show.json.jbuilder
+++ app/views/api/track_comments/show.json.jbuilder
@@ -1,3 +1,5 @@
 json.extract! @comment, :id, :body, :track_id, :author_id
 json.author @comment.author
 json.score @comment.score
+
+json.upvotes @comment.upvotes
--- frontend/components/tracks/track_comment_upvotes/comment_upvotes.jsx
+++ frontend/components/tracks/track_comment_upvotes/comment_upvotes.jsx
@@ -6,16 +6,26 @@ import React from 'react';
 class CommentUpvotes extends React.Component {
   constructor(props) {
     super(props);
+    this.upvotePressed = this.upvotePressed.bind(this);
   }
 
-  // upVotePressed() {
-  //
-  // }
+
+  upvotePressed() {
+      let currentUpvote;
+      currentUpvote = this.props.comment.upvotes.find(upvote => upvote.user_id === this.props.currentUser.id );
+
+      if (currentUpvote && currentUpvote.user_id === this.props.currentUser.id && currentUpvote.vote_value === 1) {
+        this.props.destroyUpvote(currentUpvote.id);
+      }
+      else {
+        this.props.createUpvote(this.props.comment.id, {comment_upvote: {vote_value: 1}});
+      }
+    }
 
   render() {
     return(
       <div>
-        <button onClick={ () => this.props.createUpvote(this.props.comment.id, {comment_upvote: {vote_value: 1}}) }>Upvote</button>
+        <button onClick={ () => this.upvotePressed() }>Upvote</button>
 
           {this.props.comment.score}
 
@@ -27,3 +37,46 @@ class CommentUpvotes extends React.Component {
 }
 
 export default CommentUpvotes;
+
+
+
+// import React from 'react';
+//just gotta add buttons here that call dispatch actions or whatever feel me
+//gotta do some logic here like if an upvote already has an upvote value
+// class CommentUpvotes extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.upvotePressed = this.upvotePressed.bind(this);
+//   }
+//
+// // () =>
+// //destroyUpvote
+//
+// //need to test updateUpvote
+//   upvotePressed() {
+//     let currentUpvote;
+//     currentUpvote = this.props.comment.upvotes.find(upvote => upvote.user_id === this.props.currentUser.id );
+//
+//     if (currentUpvote && currentUpvote.user_id === this.props.currentUser.id && currentUpvote.vote_value === 1) {
+//       this.props.destroyUpvote(currentUpvote.id);
+//     }
+//     else {
+//       this.props.createUpvote(this.props.comment.id, {comment_upvote: {vote_value: 1}});
+//     }
+// }
+//
+//   render() {
+//     return(
+//       <div>
+//         <button onClick={ () => this.upvotePressed() }>Upvote</button>
+//
+//           {this.props.comment.score}
+//
+//         <button>Downvote</button>
+//
+//       </div>
+//     );
+//   }
+// }
+//
+// export default CommentUpvotes;
